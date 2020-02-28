c567823736e380ebca60d291a7e8b219897d51dc
UtkarshSao <34209762+Utkarsh-Sao-mvj@users.noreply.github.com>
2020-02-28 15:27:19 +0530

    lab

--- lab5.py
+++ lab5.py
@@ -1,1 +1,18 @@
 #simulator of simple calculator +,-,/,//,*,**
+def calc(a,b,op):
+    if(op == '+'):
+        return a+b
+    elif(op == '-'):
+        return a-b
+    elif(op == '*'):
+        return a*b
+    elif(op == '/'):
+        return a/b
+    elif(op == '**'):
+        return a**b
+    elif(op == '//'):
+        return a//b
+
+a,b =[int(x) for x in input().split()]
+op = input()
+print(calc(a,b,op))
